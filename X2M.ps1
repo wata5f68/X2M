4232387d5bc119b27854238bbba683e716ee486f
unknown <fgsw@icloud.com>
2023-07-04 02:33:49 +0900

    initial commit

---
+++ X2M.ps1
@@ -0,0 +1,13 @@
+$ExcelData = Get-Clipboard  
+$ColNum = ($ExcelData[0] | Measure-Object -Word).Words
+
+$MarkdownData = (Write-Output $ExcelData | ForEach-Object{$_ -replace "`t", "|" -replace "^", "|"  -replace "$", "|"})
+
+$align ="|"
+for ($i=0; $i -lt ([int]$ColNum); $i++){
+    $align +="---|"     
+}
+
+$MarkdownData[1] =  $align + "`n" + $MarkdownData[1]
+$MarkdownData = $MarkdownData[0..($MarkdownData.Length - 2)]
+Set-Clipboard $MarkdownData
