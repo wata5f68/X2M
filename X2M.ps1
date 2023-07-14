45f4bd2b3ceda17691bf79966d436c5721a5f553
unknown <fgsw@icloud.com>
2023-07-15 06:14:36 +0900

    要素内にスペースがあっても列数を取得できるように修正

--- X2M.ps1
+++ X2M.ps1
@@ -1,5 +1,4 @@
-$ExcelData = Get-Clipboard  
-$ColNum = ($ExcelData[0] | Measure-Object -Word).Words
+$ExcelData = Get-Clipboard
 
 $MarkdownData = (Write-Output $ExcelData | ForEach-Object {$_ `
     -replace "`t", "|" `
@@ -7,6 +6,14 @@ $MarkdownData = (Write-Output $ExcelData | ForEach-Object {$_ `
     -replace "$", "|" `
     -replace "`n", "<br>" `
 })
+
+$ColNum = 0
+for ($i=0; $i -lt ($MarkdownData[0]).Length; $i++ ){
+    if ($MarkdownData[0][$i] -eq "|") {
+        $ColNum++
+    }
+}
+$ColNum--
 
 $align ="|"
 for ($i=0; $i -lt ([int]$ColNum); $i++){
@@ -18,4 +25,5 @@ $MarkdownData[1] =  $align + "`n" + $MarkdownData[1]
 
 # delete vertical bar in end
 $MarkdownData = $MarkdownData[0..($MarkdownData.Length - 2)]
+echo $MarkdownData
 Set-Clipboard $MarkdownData
